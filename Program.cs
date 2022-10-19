e71b898bf6332a2bd71f33b2a4357dc5094c2f6b
saifmhr <saifmahar165@gmail.com>
2022-10-19 19:36:04 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Saifullah";
+            char charactorV = 's';
+            int integerV = 345;
+            double doubleV = 7478387883;
+            bool boolV = false;
+
+            //print values simple
+            Console.WriteLine(name);
+            Console.WriteLine(charactorV);
+            Console.WriteLine(integerV);
+            Console.WriteLine(doubleV);
+            Console.WriteLine(boolV);
+
+            int a, b, c,d;
+            a = 22;
+            b = 33;
+            c = 44;
+
+            d = a + b - c;
+            Console.WriteLine(d);
+
+        }
+    }
+}
